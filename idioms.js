16f5d2fdfc8379ac94fdc2c1af0f24027c37aa3f
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-11-02 22:42:03 -0400

    idioms

---
+++ idioms.js
@@ -0,0 +1,15 @@
+const idioms = {
+    "A blessing in disguise": "a good thing that seemed bad at first",
+    "A dime a dozen" : "Something common",
+    "Beat around the bush" : "Avoid saying what you mean, usually because it is uncomfortable",
+    "Better late than never" : "Better to arrive late than not to come at all",
+    "Bite the bullet": "To get something over with because it is inevitable",
+    "Break a leg": "Good luck",
+    "Call it a day": "Stop working on something",
+    "Cut somebody some slack": "Don't be so critical",
+    "Cutting corners": "Doing something poorly in order to save time or money",
+    "Easy does it" : "Slow down",
+    "Get out of hand": "Get out of control",
+    "Get something out of your system": "Do the thing you've been wanting to do so you can move on",
+    
+}
