d6e6bf8340b4db5aaf880538185a8608707d6d9d
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2022-11-02 22:45:08 -0400

    progress on match functionality

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-const stringArr = ['hi', 'bye', 'hello'];
+// const stringArr = ['hi', 'bye', 'hello'];
 
-let string = 'hi there Daria'
-let checkString = stringArr[0];
+// let string = 'hi there Daria'
+// let checkString = stringArr[0];
 
-const regex = new RegExp(`${checkString}`)
+// const regex = new RegExp(`${checkString}`)
 
-string = string.replace(regex, 'hello');
+// string = string.replace(regex, 'hello');
 
-console.log(string);
+// console.log(string);
+
+const matchObj = {};
 
 const library = {
     "Whatever float's your boat" : "We can do whatever you want",
     "Monkey business" : "When something is ridiculous",
-    "It's a piece of cake" : "It's easy"
+    "It's a piece of cake" : "It's easy",
+    "Hang in there" : "don't give up"
 };
+
+const body = document.querySelector('body');
 
 //loop through the contents of the library
 for (const idiom in library) {
     //create new regex with the current idiom
     const regex = new RegExp(`${idiom}`);
-    const body = document.querySelector('body');
     bodyInnerText = body.innerText;
     const match = bodyInnerText.match(regex);
     //if any of the current idioms match with content on the web page, run the functionality for the user
     if (match) {
-        giveMeaning(idiom);
+        console.log('Match!');
+        matchObj[idiom] = library[idiom];
+        console.log(matchObj);
+        // giveMeaning(idiom, regex);   
     }
 }
 
-console.log(library);
+// function giveMeaning(idiom, regex) {
+//     elArr = document.querySelectorAll('*');
+//     //select the term in the page
+//     for (let i = 0; i < elArr.length; i++) {
+//         if (elArr[i].innerText.match(regex)) {
+//             console.log('Found the very specific match');
+//             console.log(elArr[i].innerText);
+//         }
+//     }
+//     //highlight it, insert the definition next to it
+//     //select the element and replace it with the same words but enclosed with specific style tags
+// }
+
+function popUp () {
+ return;   
+}
+
+sampleTitle = document.createElement('h1');
+sampleTitle.innerText = "HEY ITS KEVIN AND JASON"
+body.appendChild(sampleTitle);
+
+console.log('extension is working');
